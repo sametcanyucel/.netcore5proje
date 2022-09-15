b59eba524fed10765871c080646b3c0365d2b01a
sametcanyucel <113107304+sametcanyucel@users.noreply.github.com>
2022-09-15 15:59:26 +0300

    .net core proje kampı sertifika görevi

---
+++ CoreDeneme/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository categoryRepository;
+        public CategoryManager()
+        {
+            categoryRepository = new EfCategoryRepository();
+        }
+        public void AddCategory(Category category)
+        {
+            categoryRepository.Insert(category);
+        }
+
+        public List<Category> GetCategories()
+        {
+           return categoryRepository.GetListAll();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return categoryRepository.GetByID(id);
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            categoryRepository.Delete(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            categoryRepository.Update(category);
+        }
+    }
+}
---
+++ CoreDeneme/CoreDeneme/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDeneme.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
